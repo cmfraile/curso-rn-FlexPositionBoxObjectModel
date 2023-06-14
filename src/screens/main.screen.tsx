7653dc1aaebe201a558e8f878aa09a8d72646c48
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-06-14 17:40:10 +0200

    71

--- src/screens/main.screen.tsx
+++ src/screens/main.screen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet , Text, SafeAreaView, useWindowDimensions } from "react-native"
+import { StyleSheet , Text, SafeAreaView, useWindowDimensions, View } from "react-native"
 
 const MainScreen = () => {
 
@@ -6,7 +6,9 @@ const MainScreen = () => {
 
     return(
         <SafeAreaView style={styles.container}>
-            <Text style={styles.text}>W : {width} , H : {height}</Text>
+            {/*<Text style={styles.text}>W : {width} , H : {height}</Text>*/}
+            <View style={[styles.viewFirst,styles.naranja]}></View>
+            <View style={[styles.viewFirst,styles.purpura]}></View>
         </SafeAreaView>
     )
 
@@ -14,6 +16,8 @@ const MainScreen = () => {
 
 const styles = StyleSheet.create({
     container:{flex:1,justifyContent:'center',alignItems:'center'},
+    viewFirst:{width:100,height:100,borderWidth:5,borderColor:'white'},
+    naranja:{backgroundColor:'orange',left:50,position:'absolute'},purpura:{backgroundColor:'purple',position:'absolute',left:0},
     text:{fontSize:30,fontWeight:'500'}
 })
 
