a156b2b34c5568f4128f4ba77a8d955f3502c5ae
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-06-15 09:31:54 +0200

    78

--- src/screens/flexbox.screen.tsx
+++ src/screens/flexbox.screen.tsx
@@ -4,12 +4,21 @@ const FlexboxScreen = () => {
 
     //const { width , height } = useWindowDimensions()
 
-    return(
-        <SafeAreaView style={styles.container}>
-            {/*<Text style={styles.text}>W : {width} , H : {height}</Text>*/}
+    const CraftAndRender = () =>
+        <>
             <View style={[styles.viewFirst,styles.naranja]}></View>
             <View style={[styles.viewFirst,styles.purpura]}></View>
             <View style={[styles.viewFirst,styles.verde]}></View>
+            <View style={[styles.viewFirst,styles.naranja]}></View>
+            <View style={[styles.viewFirst,styles.purpura]}></View>
+            <View style={[styles.viewFirst,styles.verde]}></View>
+        </>
+
+
+    return(
+        <SafeAreaView style={styles.container}>
+            {/*<Text style={styles.text}>W : {width} , H : {height}</Text>*/}
+            <CraftAndRender/>
         </SafeAreaView>
     )
 
@@ -18,14 +27,15 @@ const FlexboxScreen = () => {
 const styles = StyleSheet.create({
     container:{
         flex:1,
+        flexWrap:'wrap',
+        flexDirection:'row'
     },
     viewFirst:{
-        minHeight:100,minWidth:100,
+        minHeight:100,minWidth:'50%',
         maxHeight:100,maxWidth:100,
         borderRadius:10,borderWidth:3,padding:10},
     naranja:{
         flex:1,
-        alignSelf:'flex-end',
         backgroundColor:'orange'
     },
     purpura:{
